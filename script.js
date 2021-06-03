bf12411f008f8bdc269aa2d84deddd8f0d5354d9
froggy <chrisdetmering@gmail.com>
2021-06-03 12:23:54 -0700

    Code review completed

--- script.js
+++ script.js
@@ -1,130 +1,96 @@
-const numberButtons = document.querySelectorAll('.number');
-const operatorButtons = document.querySelectorAll('.operator');
 const numberInput = document.querySelector('#numberinput');
-const buttonsArea = document.querySelector('#buttons');
 let firstOperand = '';
 let secondOperand = '';
 let operator = '';
-let solution = '';
 
-function drawToDisplay(button) {
-    checkForSingleZero()
-    numberInput.textContent += `${button}`;
+function drawToDisplay(value) {
+    numberInput.textContent = value;
 }
 
 function clearDisplay() {
     numberInput.textContent = '';
 }
 
-function checkForSingleZero() {
-    if (numberInput.textContent === '0' && numberInput.textContent.length === 1) {
-        numberInput.textContent = '';
+function solveEquation() {
+    const firstNumber = parseFloat(firstOperand);
+    const secondNumber = parseFloat(secondOperand);
+    if (operator === '+') {
+        return firstNumber + secondNumber;
+    };
+    if (operator === '-') {
+        return firstNumber - secondNumber;
+    };
+    if (operator === 'X') {
+        return firstNumber * secondNumber;
+    };
+    if (operator === '÷') {
+        return firstNumber / secondNumber;
     };
 }
 
-function solveOperation() {
-    firstNumber = parseFloat(firstOperand);
-    secondNumber = parseFloat(secondOperand);
-    if (firstOperand && secondOperand && operator) {
-        if (operator === '+') {
-            solution = firstNumber + secondNumber;
-        };
-        if (operator === '-') {
-            solution = firstNumber - secondNumber;
-        };
-        if (operator === 'X') {
-            solution = firstNumber * secondNumber;
-        };
-        if (operator === '÷') {
-            solution = firstNumber / secondNumber;
-        };
-    }
-    clearDisplay()
-    drawToDisplay(solution);
-    firstOperand = '';
-    secondOperand = '';
-}
-
-numberButtons.forEach(numberButton => {
+document.querySelectorAll('.number').forEach(numberButton => {
     numberButton.addEventListener('click', () => {
         const number = numberButton.textContent;
 
-        if (solution) {
-            clearDisplay();
-            solution = '';
-        }
         if (!operator) {
             firstOperand += number;
-            drawToDisplay(number);
+            drawToDisplay(firstOperand);
+            return;
         }
         if (operator) {
-            if (buttonsArea.classList.contains('operatorClicked')) {
-                clearDisplay();
-                buttonsArea.classList.remove('operatorClicked');
-            }
             secondOperand += number;
-            drawToDisplay(number);
+            drawToDisplay(secondOperand);
         }
     })
 })
 
 document.querySelector('#decimal').addEventListener('click', () => {
     if (!operator) {
-        if (firstOperand.includes('.')) {
-            return;
+        if (!firstOperand.includes('.')) {
+            firstOperand += '.';
+            drawToDisplay(firstOperand);
         }
-        firstOperand += '.';
-        drawToDisplay('.');
     }
+
     if (operator) {
-        if (secondOperand.includes('.')) {
-            return;
+        if (!secondOperand.includes('.')) {
+            secondOperand += '.';
+            drawToDisplay(secondOperand);
         }
-        if (buttonsArea.classList.contains('operatorClicked')) {
-            clearDisplay()
-            buttonsArea.classList.remove('operatorClicked')
-        }
-        secondOperand += '.';
-        drawToDisplay('.');
     }
-    if (solution) {
-        clearDisplay();
-        drawToDisplay('.');
-        solution = '';
-    }
+
 });
 
 document.querySelector('#clear').addEventListener('click', () => {
-    if (firstOperand && !secondOperand) {
-        firstOperand = '';
-    }
-    if (secondOperand) {
-        secondOperand = '';
-    }
-    clearDisplay()
+    firstOperand = '';
+    operator = '';
+    secondOperand = '';
+    drawToDisplay('0');
 })
 
-operatorButtons.forEach(operatorButton => {
+document.querySelectorAll('.operator').forEach(operatorButton => {
     operatorButton.addEventListener('click', (event) => {
         const selectedOperator = event.target.textContent;
-        if (firstOperand && secondOperand) {
-            solveOperation()
-            firstOperand = solution;
+        if (secondOperand) {
+            const result = solveEquation();
+            firstOperand = result;
+            secondOperand = '';
+            operator = selectedOperator;
+            drawToDisplay(result);
+            return;
         }
         if (firstOperand) {
-            solution = '';
             operator = selectedOperator;
         }
-        if (solution) {
-            firstOperand = solution;
-            operator = selectedOperator;
-            clearDisplay()
-        }
-        buttonsArea.classList.add('operatorClicked');
     })
 })
 
 document.querySelector('#equals').addEventListener('click', () => {
-    solveOperation();
-    operator = '';
+    if (secondOperand) {
+        const result = solveEquation();
+        firstOperand = result;
+        secondOperand = '';
+        operator = '';
+        drawToDisplay(result);
+    }
 })
